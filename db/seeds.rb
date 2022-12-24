f7eca6d8ad70006239da8ba02a6bc25bceede58f
tkyg <80114433+tkyg@users.noreply.github.com>
2022-12-24 09:20:50 -0500

    seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "🐕 Seeding data..."
+
+User.destroy_all
+
+test_user_1 = User.create(username: "Test 1", password: "1234")
+test_user_2 = User.create(username: "Test 2", password: "4321")
+
+puts "✅ Done seeding!"
