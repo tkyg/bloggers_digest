5d0401046988a874952716ce4e8c2eb146022503
tkyg <80114433+tkyg@users.noreply.github.com>
2022-12-24 09:32:12 -0500

    migrate and seed blog table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :blogs 
   get "/signup", to: "users#create"
   # resources :users
   # Routing logic: fallback requests for React Router.
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,13 @@
 puts "🐕 Seeding data..."
 
 User.destroy_all
+Blog.destroy_all
 
 test_user_1 = User.create(username: "Test 1", password: "1234")
 test_user_2 = User.create(username: "Test 2", password: "4321")
+
+b1 = Blog.create(title: "Wine Pairing: A Beginner's Guide", content: "Pairing wine with food...")
+b2 = Blog.create(title: "Photography 101", content: "Clicking a good picture...")
+b3 = Blog.create(title: "Beef Dumpling Recipe", content: "Dumplings are my favorite...")
 
 puts "✅ Done seeding!"
