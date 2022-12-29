27c9575afc6580695336c80489deec00cebd11da
tkyg <80114433+tkyg@users.noreply.github.com>
2022-12-29 09:01:27 -0500

    allow users to sign-up, and sign-in through before_action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
-
+  skip_before_action :authenticate_user, only: [:create]
+  
   def create
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
