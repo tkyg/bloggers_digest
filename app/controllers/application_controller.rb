35ba619d76b7029950a31516d72f90d0a10c1640
tkyg <80114433+tkyg@users.noreply.github.com>
2022-12-29 08:58:07 -0500

    limit accessibility unless valid user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::API
-
+  before_action :authenticate_user
+  
   include ActionController::Cookies
 
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
+  end
+
+  private
+
+  def authenticate_user
+    render json: { errors: {User: "Not Authorized"}}, status: :unauthorized unless current_user
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
 
+  skip_before_action :authenticate_user, only: [:create]
+
+  def show
+    user = User.find(params[:id])
+    render json: user, status: :ok
+  end
+
   def create
     user = User.new(user_params)
     if user.save
