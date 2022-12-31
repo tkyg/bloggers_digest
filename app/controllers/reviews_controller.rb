3759e6a4e5ad5ec9ecc86a6cb8139cabbcc382ea
tkyg <80114433+tkyg@users.noreply.github.com>
2022-12-31 11:04:28 -0500

    create accessibility for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+skip_before_action :authenticate_user, only: [:index, :show, :create, :update, :destroy]
 
   def index
     render json: Review.all, status: :ok
@@ -11,6 +12,21 @@ class ReviewsController < ApplicationController
     else
       render json: { error: "Review not found" }, status: :not_found
     end
+  end
+
+  def create
+    review = Review.new(review_params)
+    if review.save
+      render json: review, status: :created
+    else
+      render json: {errors: review.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.permit(:likes, :comment)
   end
 
   
