0839b3ac526ba4b21751b2bf50f90be1ea97a363
tkyg <80114433+tkyg@users.noreply.github.com>
2023-01-02 09:19:49 -0500

    delete accessibility for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -23,6 +23,26 @@ skip_before_action :authenticate_user, only: [:index, :show, :create, :update, :
     end
   end
 
+  def update
+    review = Review.find_by(id:params[:id])
+    if review
+      review.update(review_params)
+      render json: review, status: :accepted
+    else
+      render json: { error: "Review not found" }, status: :not_found
+    end
+  end
+
+  def destroy
+    review = Review.find_by(id:params[:id])
+    if review
+      review.destroy
+      head :no_content
+    else
+      render json: { error: "Review not found" }, status: :not_found
+    end
+  end
+
   private
 
   def review_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   resources :reviews, only: [:index, :show, :create, :update, :destroy]
   resources :blogs, only: [:index, :show, :create, :update, :destroy]
 
+  patch "/reviews/:id/likes", to: "reviews#increment_likes"
   post "/login", to: 'sessions#create'
   delete "/logout", to: 'sessions#destroy'
 
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
-  
+
   # resources :users
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
