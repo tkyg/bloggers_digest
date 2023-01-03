fc4e29222b2c4b2905ec36fc1befe94703c29e03
tkyg <80114433+tkyg@users.noreply.github.com>
2023-01-03 16:46:04 -0500

    seperating logged in and out user access

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,22 +2,35 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 
-const NavBar = () => {
+const NavBar = ({ loggedIn }) => {
 
   const handleLogout = e => {
     e.preventDefault();
   }
 
+  const loggedInRoutes = () => {
+    return(
+      <>
+        <li><Link to="/profile">Profile</Link></li>
+        <li><a href="#" onClick={ handleLogout }>Logout</a></li>
+      </>
+    )
+  }
 
+  const loggedOutRoutes = () => {
+    return(
+      <>
+        <li><Link to="/signup">Signup</Link></li>
+        <li><Link to="/login">Login</Link></li>
+      </>
+    )
+  }
 
   return (
     <ul>
-    <li><Link to="/">Home</Link></li>
-    <li><Link to="/signup">Signup</Link></li>
-    <li><Link to="/login">Login</Link></li>
-    <li><a href="#" onClick={ handleLogout }>Logout</a></li>
-    
-  </ul>
+      <li><Link to="/">Home</Link></li>
+      { loggedIn ? loggedInRoutes() : loggedOutRoutes() }
+    </ul>
   )
 }
 
