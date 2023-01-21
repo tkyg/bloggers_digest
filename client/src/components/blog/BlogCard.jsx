cb0e9fa66d5e970a0a14219ca8e23780e6f171f5
tkyg <80114433+tkyg@users.noreply.github.com>
2023-01-21 07:53:52 -0500

    allow users to view a single blog

---
+++ client/src/components/blog/BlogCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { NavLink} from "react-router-dom";
+import "./blogCard.css"
+
+const BlogCard = ({blog}) => {
+  return (
+    <div className="blog">
+      <div className="blogInfo">
+        <NavLink to={`/blogs/${blog.id}`}>
+          <span className="blogTitle" style={{textDecoration: "none"}}>
+            {blog.title}
+          </span>
+        </NavLink>
+        <p className='blogContent'>{blog.content}</p>
+        <span className='blogAuthor'>
+          Written By: {blog.user.username}
+        </span>
+        <br/>
+      </div>
+    </div>
+    )
+  }
+  
+  export default BlogCard
