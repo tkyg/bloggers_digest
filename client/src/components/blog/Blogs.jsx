0473c1ae6ecb9f3066e774e3a6e93bb1f2f74333
tkyg <80114433+tkyg@users.noreply.github.com>
2023-01-21 07:55:50 -0500

    allow users to view only their blogs

---
+++ client/src/components/blog/Blogs.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react'
+import { UserContext } from '../context/User'
+import BlogCard from '../blog/BlogCard'
+import "./blogs.css"
+
+
+const Blogs = () => {
+  const { blogs, loggedIn } = useContext(UserContext)
+
+  if(loggedIn) {
+    const blogsList = blogs.map((blog, index) => <BlogCard key={index} blog={blog}/>)
+  return (
+    <div className='blogs'>
+      <br/>
+      <span>
+        {blogsList}
+      </span>
+    </div>
+  )
+} else {
+  return (<h3>Please Login or Signup to Read and Post a Blog</h3>)
+}
+}
+
+export default Blogs
